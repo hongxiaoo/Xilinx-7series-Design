a96514c851463bf01a6a720b42d91c7aa66e1ce4
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-19 02:53:59 +0000

    Update arthmatic_logic.sv

--- arthmatic_logic.sv
+++ arthmatic_logic.sv
@@ -7,5 +7,56 @@ module add_sub #(
   input  logic [DATA_WIDTH-1:0] b,
   output logic [DATA_WIDTH-0:0] sum
 );
+  
+  always_ff @(posedge clk) begin
+    sum <= a + ({DATA_WIDTH{mode}}^b) + mode;
+  end
 
+endmodule
+
+// 2-stage pipelined MULTIPLIER - uses 1-DSP48
+module int_mul #(
+  parameter DATA_WIDTH = 8
+) (
+  input  logic clk,
+  input  logic [DATA_WIDTH-1:0] a,
+  input  logic [DATA_WIDTH-1:0] b,
+  output logic [2*DATA_WIDTH-1:0] mul
+);
+  
+  logic signed [DATA_WIDTH-1:0] a_reg;
+  logic signed [DATA_WIDTH-1:0] b_reg;
+  always_ff @(posedge clk) begin
+    a_reg <= a;
+    b_reg <= b;
+    mul <= a_reg * b_reg;
+  end
+
+endmodule
+// 2-stage pipelined load-enabled MAC - uses 1-DSP48
+module int_mac #(
+  parameter DATA_WIDTH = 8
+) (
+  input  logic clk,
+  input  logic load,
+  input  logic [DATA_WIDTH-1:0] a,
+  input  logic [DATA_WIDTH-1:0] b,
+  output logic [2*DATA_WIDTH-1:0] acc
+);
+  
+  logic signed [DATA_WIDTH-1:0] a_reg;
+  logic signed [DATA_WIDTH-1:0] b_reg;
+  logic signed [2*DATA_WIDTH-1:0] mul_reg;
+  logic signed [2*DATA_WIDTH-1:0] acc_int;
+  logic signed [2*DATA_WIDTH-1:0] acc_reg;
+  
+  assign acc_int = load ? {2*DATA_WIDTH{1'b0}} : acc_reg;
+  always_ff @(posedge clk) begin
+    a_reg <= a;
+    b_reg <= b;
+    mul_reg <= a_reg * b_reg;
+    acc_reg <= acc_int + mul_reg;
+  end
+  assign acc = acc_reg;
+  
 endmodule
