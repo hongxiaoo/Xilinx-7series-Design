9a835985cd832708fd63a7a7594834a0431521a7
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-19 03:10:54 +0000

    Update synchronizer.sv

--- synchronizer.sv
+++ synchronizer.sv
@@ -7,10 +7,31 @@ module pipeline_reg #(
   output logic dout
 );
 
-  logic [SYNC_STAGE-1:0] data;
+  logic [SYNC_STAGE-1:0] sync_reg;
   always_ff @(posedge clk) begin
-    data <= {data[SYNC_STAGE-2:0],din};
+    sync_reg <= {sync_reg[SYNC_STAGE-2:0],din};
   end
-  assign dout = data[SYNC_STAGE-1];
+  assign dout = sync_reg[SYNC_STAGE-1];
+  
+endmodule
+// Reset-Sync : Async-assert and sync-deassert
+// Min 3 stage pipeline to mitegate reset recovery and removal time
+module reset_sync #(
+  parameter SYNC_STAGE = 3
+) (
+  input  logic clk,
+  input  logic async_reset,
+  output logic sync_reset
+);
+  
+  (* ASYNC_REG = "TRUE" *) logic [SYNC_STAGE-1:0] sync_reg;
+  always_ff @(posedge clk or posedge async_reset) begin
+    if (async_reset) begin
+      sync_reg <= {SYNC_STAGE{1'b1}};
+    end else begin
+      sync_reg <= {sync_reg[SYNC_STAGE-2:0],1'b0};
+    end
+  end
+  assign sync_reset = sync_reg[SYNC_STAGE-1];
   
 endmodule
