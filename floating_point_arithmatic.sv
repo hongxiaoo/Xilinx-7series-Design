e44914a9e451c9ecf549cfd71d103c6a9f901ff7
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-21 19:10:46 +0000

    Create floating_point_arithmatic.sv

---
+++ floating_point_arithmatic.sv
@@ -0,0 +1,10 @@
+// Snthesisable code for IEEE754 SP-floating point arithmatic using DSP48 blocks 
+// Single-precision Floating Point[IEEE754] Multipler
+module floating_point_multipler (
+  input  logic clk,
+  input  logic [31:0] a,
+  input  logic [31:0] b,
+  output logic [31:0] p
+);
+
+endmodule
