495f7f1eb50fd5b1f4bdd0ac3bf12c6476597e20
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-21 18:34:05 +0000

    Update memory_design.sv

--- memory_design.sv
+++ memory_design.sv
@@ -4,6 +4,7 @@
 // if the DATA_WIDTH > 16 bit use Block-RAM
 module spram #(
   parameter RAM_STYLE = "distributed"
+  parameter REG_RPORT = 1,  // Async-read supported only in distributed RAM
   parameter DATA_WIDTH = 2, // > 16 need to use BRAM
   parameter ADDR_WIDTH = 5
   ) (
@@ -13,5 +14,57 @@ module spram #(
   input  logic [DATA_WIDTH-1:0] din,
   output logic [DATA_WIDTH-1:0] dout
   );
+  
+  (* ram_style = RAM_STYLE *)
+  logic [DATA_WIDTH-1:0] mem [2**ADDR_WIDTH];
+  always_ff @(posedge clk) begin
+    if (we) begin
+      mem[addr] <= din;
+    end
+  end
+  generate if (REG_RPORT) begin
+    always_ff @(posedge clk) begin
+      dout <= mem[addr];
+    end
+  end else begin
+    assign dout = mem[addr];
+  end endgenerate
+  
+endmodule
+
+module dpram #(
+  parameter RAM_STYLE = "distributed"
+  parameter REG_RPORT = 1,  // Async-read supported only in distributed RAM
+  parameter DATA_WIDTH = 2, // > 16 need to use BRAM
+  parameter ADDR_WIDTH = 5
+  ) (
+  input  logic aclk,
+  input  logic awe,
+  input  logic [ADDR_WIDTH-1:0] aaddr,
+  input  logic [DATA_WIDTH-1:0] adin,
+  output logic [DATA_WIDTH-1:0] adout,
+  input  logic bclk,
+  input  logic [ADDR_WIDTH-1:0] baddr,
+  output logic [DATA_WIDTH-1:0] bdout
+  );
+  
+  (* ram_style = RAM_STYLE *)
+  logic [DATA_WIDTH-1:0] mem [2**ADDR_WIDTH];
+  always_ff @(posedge aclk) begin
+    if (awe) begin
+      mem[aaddr] <= adin;
+    end
+  end
+  generate if (REG_RPORT) begin
+    always_ff @(posedge aclk) begin
+      adout <= mem[aaddr];
+    end
+    always_ff @(posedge bclk) begin
+      bdout <= mem[baddr];
+    end
+  end else begin
+    assign adout = mem[aaddr];
+    assign bdout = mem[baddr];
+  end endgenerate
   
 endmodule
